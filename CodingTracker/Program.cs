605878a1887ea00cf9bad80b5ea21a5a5d9ac12b
JavierAD742 <146769209+JavierAD742@users.noreply.github.com>
2026-08-15 21:50:24 -0400

    Keeping one message
    Removed duplicate console write lines for clarity.

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,7 +1,2 @@
 ﻿Console.WriteLine("Hello, C# Academy! One more modification!");
-Console.WriteLine("Hello, C# Academy! One more modification!");
-Console.WriteLine("Hello, C# Academy! One more modification!");
-Console.WriteLine("Hello, C# Academy! One more modification!");
-Console.WriteLine("Hello, C# Academy! One more modification!");
-Console.WriteLine("Hello, C# Academy! One more modification!");
-Console.WriteLine("Hello, C# Academy! One more modification!");
+
