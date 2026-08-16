688b3788f6a23c5f42a2dc51aeb1a1bbb260ec2d
JavierAD742 <146769209+JavierAD742@users.noreply.github.com>
2026-08-15 22:02:39 -0400

    Initial Commit

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,6 +1,15 @@
 ﻿
 MyMethod();
-void MyMethod()
+void MyFirstMethod()
+{
+    Console.WriteLine("Now I have a method")
+}
+
+void MySecondMethod()
+{
+    Console.WriteLine("Now I have a method")
+}
+void MyThirdMethod()
 {
     Console.WriteLine("Now I have a method")
 }
